1ba35452516fb2c0218d467fd22c637b651f5cf9
Pedro Kayami <pedro.k.franchini@gmail.com>
2024-05-05 12:23:58 -0300

    Api rest using .Net

---
+++ Crud/StudyCrudAPI/StudyCrudAPI/Controllers/DevEventsController.cs
@@ -0,0 +1,86 @@
+﻿using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+using StudyCrudAPI.Entities;
+using StudyCrudAPI.Persistence;
+
+namespace StudyCrudAPI.Controllers
+{
+    [Route("api/dev-events")]
+    [ApiController]
+    public class DevEventsController : ControllerBase
+    {
+        private readonly DevEventsDbContext _context;
+
+        public DevEventsController(DevEventsDbContext context)
+        {
+            _context = context;
+        }
+
+        [HttpGet]
+        public IActionResult GetAll()
+        {
+           var devEvents = _context.DevEvents.Where(id => !id.IsDeleted).ToList();
+            return Ok(devEvents);
+        }
+
+        [HttpGet("{id}")]
+        public IActionResult GetbyId(Guid id)
+        {
+            var devEvent = _context.DevEvents.FirstOrDefault(d => d.id == id);
+            if (devEvent == null)
+            {
+                return NotFound();
+            }
+            return Ok(devEvent);
+        }
+
+        [HttpPost]
+        public IActionResult Post(DevEvent devEvent)
+        {
+            _context.DevEvents.Add(devEvent);
+            return CreatedAtAction(nameof(GetbyId), new { id = devEvent.id }, devEvent);
+        }
+
+        [HttpPut("{id}")]
+        public IActionResult Update(Guid id,DevEvent input)
+        {
+            var devEvent = _context.DevEvents.FirstOrDefault(d => d.id == id);
+            if (devEvent == null)
+            {
+                return NotFound();
+            }
+
+            devEvent.Update(input.Title, input.Description, input.StartDate, input.EndDate);
+
+            return NoContent();
+        }
+
+        [HttpDelete("{id}")]
+        public IActionResult Delete(Guid id)
+        {
+            var devEvent = _context.DevEvents.FirstOrDefault(d => d.id == id);
+            if (devEvent == null)
+            {
+                return NotFound();
+            }
+
+            devEvent.Delete();
+
+            return NoContent();
+        }
+
+        [HttpPost("{id}/speakers")]
+        public IActionResult PostSpeaker(Guid id, DevEventsSpeaker speaker)
+        {
+            var devEvent = _context.DevEvents.FirstOrDefault(d => d.id == id);
+
+            if (devEvent == null)
+            {
+                return NotFound();
+            }
+
+            devEvent.Speakers.Add(speaker);
+            return NoContent();
+        }
+    }
+}
---
+++ Crud/StudyCrudAPI/StudyCrudAPI/Persistence/DevEventsDbContext.cs
@@ -0,0 +1,14 @@
+﻿using StudyCrudAPI.Entities;
+
+namespace StudyCrudAPI.Persistence
+{
+    public class DevEventsDbContext
+    {
+        public List<DevEvent> DevEvents { get; set; }
+
+        public DevEventsDbContext()
+        {
+            DevEvents = new List<DevEvent>();
+        }
+    }
+}
